a749485cf3a57c24a6c729957002ca8996ab6fd6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-04-30 18:39:23 +0900

    レイアウトを微修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,15 @@
 import Head from "next/head";
 import { Main } from "src/components/Main";
-import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h2 className={styles.title}>この話題で話してね！！！！</h2>
-
-        <Main />
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Main />
     </div>
   );
 }
