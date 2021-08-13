1ef7d56842f9bda73f155de909a3f30adc270973
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-13 10:16:05 +0900

    firestoreにDB登録（未完）

--- src/components/RegisterTopic/index.tsx
+++ src/components/RegisterTopic/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import { firebaseDB } from "../../firebase";
+import firebase from "firebase/app";
 
 // useForm の型指定
 interface ValueType {
@@ -19,9 +21,47 @@ export const RegisterTopic: React.VFC = () => {
     },
   });
 
+  // todo: React Hook Form を利用しないパターン 登録されない
+  // Firebaseのメソッドとかに問題ありそう
+  //https://firebase.google.com/docs/firestore/manage-data/add-data?authuser=0#web-v8_1
+  const [topic, setTopic] = useState<string>();
+  const sendTopic = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    firebaseDB
+      .collection("topics")
+      .doc("topic")
+      .set({
+        topic: topic,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        console.log("成功");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    console.log(topic);
+    setTopic("");
+  };
+
   // Submitボタン押した時の成功時？
   const handleOnSubmit: SubmitHandler<ValueType> = (values) => {
+    // todo: なぜかfirestoreに登録されない
+    firebaseDB.collection("topics").add({
+      topic: values.topic,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // .then((r) => {
+    //   // 成功
+    //   console.log(r);
+    // })
+    // .catch((e) => {
+    //   // 失敗
+    //   console.log(e);
+    // });
     console.log(values);
+    // 文字を初期化したい
   };
 
   // Submitボタン押した時の失敗時？
@@ -31,6 +71,32 @@ export const RegisterTopic: React.VFC = () => {
 
   return (
     <div>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8"
+        onSubmit={sendTopic}
+      >
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="topic"
+        >
+          React Hook Formじゃない方
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="topic"
+          type="text"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          disabled={!topic}
+        >
+          登録
+        </button>
+      </form>
+
       <form
         className="bg-white shadow-md rounded px-8 pt-6 pb-8"
         // handleSubmit(成功時処理, 失敗時処理) っぽい
