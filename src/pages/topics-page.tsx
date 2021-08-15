85e5ede4354d501281446cbc6bf62c248d5f3e97
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-15 09:24:37 +0900

    話題一覧ページタイトル修正

--- src/pages/topics-page.tsx
+++ src/pages/topics-page.tsx
@@ -5,7 +5,12 @@ import { Topics } from "../components/Topics";
 export default function TopicsPage() {
   return (
     <div>
-      <RegisterTopic />
+      <h1 className="text-xl text-center mt-5 font-bold text-blue-500">
+        新規トピック登録ページ
+      </h1>
+      <div className="pb-4">
+        <RegisterTopic />
+      </div>
       <Topics />
     </div>
   );
