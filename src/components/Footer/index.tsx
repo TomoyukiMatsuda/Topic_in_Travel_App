9786e7f0c6cf9a43889cd601d6c4021206c6291c
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-22 12:24:55 +0900

    該当ページにいることを判定してボタンフォーカス

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useCallback, memo } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const footerItems = [
   {
@@ -7,29 +8,44 @@ const footerItems = [
     label: "シャッフル",
   },
   {
-    href: "/topic-page",
-    label: "カテゴリー",
+    href: "/topics-page",
+    label: "一覧",
   },
   {
-    href: "/speaker",
+    href: "/speaker", // todo ページを用意
     label: "話す人",
   },
   {
-    href: "/login",
+    href: "/login", // todo ページを用意
     label: "ログイン",
   },
 ];
 
-export const Footer: React.VFC = () => {
+export const Footer: React.VFC = memo(() => {
+  const router = useRouter();
+  const decideItemColor = useCallback(
+    (href: string): string => {
+      // パスと一致 ? 青 : 黄
+      return href == router.pathname ? "blue" : "yellow";
+    },
+    [router.pathname]
+  );
+
   return (
     <footer className="h-12 bg-blue-300 grid grid-cols-4 text-center">
       {footerItems.map((item) => (
         <Link href={item.href}>
-          <button className="bg-yellow-400 hover:bg-yellow-300 text-white font-bold">
+          <button
+            className={`bg-${decideItemColor(
+              item.href
+            )}-400 hover:bg-${decideItemColor(
+              item.href
+            )}-300 text-white font-bold`}
+          >
             <a>{item.label}</a>
           </button>
         </Link>
       ))}
     </footer>
   );
-};
+});
