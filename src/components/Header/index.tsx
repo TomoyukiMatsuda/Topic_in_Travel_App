9a5a7e2d6c8fe5b354a055c3f4b499736669bbf9
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-22 23:33:01 +0900

    ログイン、ログアウトの同線をフッターに追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ interface Props {
 // ヘッダーコンポーネント
 export const Header: React.VFC<Props> = memo((props) => {
   return (
+    // todo トピック一覧の時に高さが小さくなるのを修正
     <header className="flex items-center justify-center h-12 bg-blue-400 items-center text-white font-bold">
       <h1>{props.title}</h1>
     </header>
