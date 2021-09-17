7ae38c806132fcc0b0566fcff6c188b58344459c
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-09-18 08:53:29 +0900

    メインボタンカラーの濃さを修正

--- src/components/Main/MainButton/index.tsx
+++ src/components/Main/MainButton/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const MainButton: React.VFC<Props> = memo((props) => {
   return (
     <button
-      className={`w-1/2 font-bold text-2xl bg-${props.color}-400 hover:bg-${props.color}-300 py-2 px-4 rounded-xl text-white mx-auto mt-1`}
+      className={`w-1/2 font-bold text-2xl bg-${props.color}-500 hover:bg-${props.color}-300 py-2 px-4 rounded-xl text-white mx-auto mt-2`}
       onClick={props.onClickFunc}
     >
       {props.children}
