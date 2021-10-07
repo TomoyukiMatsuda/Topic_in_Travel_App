c1adec81176ec633b2da2030561efb333769b0e7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-10-07 13:39:37 +0900

    userStateを一旦作成

--- src/lib/authUserContextProvider.ts
+++ src/lib/authUserContextProvider.ts
@@ -1,9 +1,17 @@
 import { createContext } from "react";
 import { AuthUser } from "../pages/_app";
+import { atom } from "recoil";
 
+// todo: コンテキストをrecoilに置き換え
 export const AuthUserContext = createContext({
   id: "",
   isAdmin: false,
   name: "",
   email: "",
 } as AuthUser);
+
+// todo: keyを作成
+export const userState = atom({
+  key: "userState",
+  default: { id: "", isAdmin: false, name: "", email: "" } as AuthUser,
+});
