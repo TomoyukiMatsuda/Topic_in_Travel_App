5e976102609890b1e41d766591bc6ac97b31ad0f
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-23 11:40:26 +0900

    topicsActionを修正

--- src/components/Topics/index.tsx
+++ src/components/Topics/index.tsx
@@ -2,6 +2,9 @@ import React, { memo, useEffect, useState } from "react";
 import { TopicItem } from "../TopicItem";
 import { Topic, useDbFromFirestore } from "../../hooks/useDbFromFirestore";
 import { firebaseDB } from "../../firebase";
+import { topicsAtom } from "../../states/topics/topicsState";
+import { useRecoilValue } from "recoil";
+import { topicsActions } from "../../states/topics/topicsActions";
 
 // 話題一覧画面コンポーネント
 export const Topics: React.VFC = memo(() => {
@@ -10,13 +13,20 @@ export const Topics: React.VFC = memo(() => {
 
   const [db, setDb] = useState<Topic[] | null>(null);
 
+  const recoilTopics = useRecoilValue(topicsAtom);
+  const topicsAction = topicsActions.useSetTopics();
+
+  console.log(recoilTopics);
+
   useEffect(() => {
-    // todo ユーザーに紐づいたtopicsだけをゲットしたい
+    // todo ユーザーに紐づいたtopicsだけをゲットできるようにしたい
     firebaseDB
       .collection("topics")
       .orderBy("timestamp", "desc")
       .get()
       .then((snapshot) => {
+        topicsAction(snapshot.docs);
+
         setDb(
           snapshot.docs.map<Topic>((doc) => ({
             id: doc.id,
--- src/states/topics/topicsActions.ts
+++ src/states/topics/topicsActions.ts
@@ -1,9 +1,12 @@
 import { useRecoilCallback } from "recoil";
 import { topicsAtom } from "./topicsState";
 import { Topic } from "../../hooks/useDbFromFirestore";
+import firebase from "firebase";
 
 export interface TopicsActions {
-  useSetTopics: () => (topics: Topic[]) => void;
+  useSetTopics: () => (
+    topicDocs: firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>[]
+  ) => void;
   useResetTopics: () => () => void;
 }
 
@@ -11,8 +14,15 @@ export const topicsActions: TopicsActions = {
   useSetTopics: () =>
     useRecoilCallback(
       ({ set }) =>
-        (topics) =>
-          set(topicsAtom, topics)
+        (topicDocs) =>
+          set(
+            topicsAtom,
+            topicDocs.map<Topic>((doc) => ({
+              id: doc.id,
+              content: doc.data().topic,
+              timestamp: doc.data().timestamp,
+            }))
+          )
     ),
   useResetTopics: () =>
     useRecoilCallback(
